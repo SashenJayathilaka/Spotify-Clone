161c691096e2f581da1e3619afcd3944aca93c05
11134212 <chilikenewbekov@gmail.com>
2023-01-16 13:31:36 +0300

    spotify2

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -1,11 +1,11 @@
-import { atom } from "recoil";
+import { atom } from 'recoil'
 
 export const playlistState = atom({
-  key: "playlistState",
-  default: null,
-});
+	key: 'playlistState',
+	default: null,
+})
 
 export const playlistIdState = atom({
-  key: "playlistIsState",
-  default: "37i9dQZF1DWWvoJqVv7uOD",
-});
+	key: 'playlistIdState',
+	default: '3mzDAFxZSfuMxpsQjneHkg',
+})
